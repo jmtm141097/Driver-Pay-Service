d57b7c7fde1fb86bf0d7e8642370edc82cabe6ae
Juan Manuel Tamayo Monje <jmtm141097@gmail.com>
2022-10-27 16:53:57 -0500

    add securtity

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ import * as dotenv from 'dotenv'
 import express from 'express'
 import compression from 'compression'
 import rutas from './routes/index.routes.js'
+import cors from 'cors'
+import helmet from 'helmet'
 
 dotenv.config()
 const app = express()
 
 app.use(compression())
 app.use(express.json())
+app.use(cors())
+app.use(helmet())
 
 app.get('/', async (_, res) => {
     res.json({
